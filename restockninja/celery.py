98dd5ef507f165237fe47bd59c07ca545024083c
Kenning Brichford <kenning@brichford.com>
2020-04-19 09:18:30 -0400

    Copy files project files from previous version

---
+++ restockninja/celery.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, unicode_literals
+from celery import Celery
+
+app = Celery('restockninja')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+    
--- restockninja/urls.py
+++ restockninja/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # path('', include('pages.urls')),
+    path('', include('pages.urls')),
 ]
 
 if settings.DEBUG:
