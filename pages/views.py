f06ce1217151d53edde6bada1732dbf1af9a23b6
Kenning Brichford <kenning@brichford.com>
2020-04-20 08:33:06 -0400

    Convert home page to template view

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, 'pages/index.html')
+class IndexView(TemplateView):
+    template_name = 'pages/index.html'
