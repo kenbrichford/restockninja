0530bcd328e1bfe5d48eeee57ec79fec02eeed74
Kenning Brichford <kenning@brichford.com>
2020-04-20 12:47:44 -0400

    Make brand slug unique and auto generate on save

--- products/models.py
+++ products/models.py
@@ -67,7 +67,12 @@ class Category(MPTTModel, Base):
 
 class Brand(models.Model):
     name = models.CharField(max_length=20)
-    slug = models.SlugField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+
+        return super(Category, self).save(*args, **kwargs)
