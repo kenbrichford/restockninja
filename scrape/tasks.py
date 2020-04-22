85d684d0d97747a099ef4ac9bcc9d9d435cfa03d
Kenning Brichford <kenning@brichford.com>
2020-04-22 09:36:13 -0400

    Remove or comment out scraping methods

--- scrape/tasks.py
+++ scrape/tasks.py
@@ -1,97 +1,39 @@
 from __future__ import absolute_import, unicode_literals
-import time
-import requests
-import threading
 from celery import shared_task
 from django.utils import timezone
-from django.core.management.base import BaseCommand
 from datetime import timedelta
-from listings.models import Listing, Price, Vendor
-from .stores import import_stores
 
-def upload_data(listing, parsed_data):
-    listing.url = parsed_data.url
+# @shared_task
+# def scrape(vendor_name, path=None):
+#     if not path:
+#         one_minute_ago = timezone.now() - timedelta(minutes=1)
 
-    last_price = Price.objects.filter(listing=listing).order_by('-updated_time').first()
+#         listings = Listing.objects.filter(updated_time__lte=one_minute_ago, vendor__name=vendor_name)\
+#             .order_by('updated_time')
 
-    if last_price and last_price.price == parsed_data.price\
-        and last_price.shipping == parsed_data.shipping:
-        last_price.save()
+#     if listings:
+#         store = create_store(vendor_name)
 
-    else:
-        price = Price.objects.create(
-            listing = listing,
-            price = parsed_data.price,
-            shipping = parsed_data.shipping,
-            availability = parsed_data.availability
-        )
+#         truncated_listings = {i.sku: i for i in listings[:store.items_per_request]}
+
+#         skus = ''.join([listing.sku for listing in listings[:store.items_per_request]])
+
+#         url = store.create_sku_url(skus)
         
-        price.save()
-
-    listing.save()
-
-def create_store(vendor_name):
-    if vendor_name == 'Best Buy':
-        store = import_stores.BestBuy()
-    
-    elif vendor_name == 'Walmart':
-        store = import_stores.Walmart()
-    
-    elif vendor_name == 'Target':
-        store = import_stores.Target()
-    
-    return store
-
-@shared_task
-def scrape(vendor_name, path=None):
-    if not path:
-        one_minute_ago = timezone.now() - timedelta(minutes=1)
-
-        listings = Listing.objects.filter(updated_time__lte=one_minute_ago, vendor__name=vendor_name)\
-            .order_by('updated_time')
-    
-    store = create_store(vendor_name)
-
-    if listings:
-        truncated_listings = {i.sku: i for i in listings[store.items_per_request]}
-
-        url = store.create_url(truncated_listings)
+#         response = requests.get(url.endpoint, params=url.params)
         
-        response = requests.get(url.endpoint, params=url.params)
+#         items = store.get_items(response.json())
         
-        items = store.get_items(response.json())
-        
-        if items:
-            for item in items:
-                parsed_data = store.parse_data(item)
+#         if items:
+#             for item in items:
+#                 parsed_data = store.parse_data(item)
                 
-                listing = truncated_listings.pop(parsed_data.sku)
+#                 listing = truncated_listings.pop(parsed_data.sku)
                 
-                upload_data(listing, parsed_data)
+#                 upload_data(listing, parsed_data)
         
-        else:
-            print('No items were returned from this request: %s' % response.text, flush=True)
+#         else:
+#             print('No items were returned from this request: %s' % response.text, flush=True)
 
-        for listing in list(truncated_listings.values()):
-            listing.save()
-        
-def single_scrape(vendor_name, path):
-    store = create_store(vendor_name)
-
-    url = store.create_url()
-        
-    response = requests.get(url.endpoint, params=url.params)
-    
-    items = store.get_items(response.json())
-    
-    if items:
-        for item in items:
-            parsed_data = store.parse_data(item)
-
-def get_sku(pattern, url):
-    try:
-        sku = re.search(pattern, url.path).group(0)
-    except AttributeError:
-        sku = None
-    
-    return sku
+#         for listing in list(truncated_listings.values()):
+#             listing.save()
