6fd15bb21ccefa479b0487413178ff6edc602a2e
Kenning Brichford <kenning@brichford.com>
2020-04-22 14:40:40 -0400

    Create stores list to get domain, vendor_name, and sku from a url

--- search/views.py
+++ search/views.py
@@ -1,9 +1,27 @@
-from urllib.parse import unquote, urlparse 
+import re
+from datetime import timedelta
+from urllib.parse import unquote, urlparse
+from django.utils import timezone
 from django.shortcuts import render, redirect
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from products.models import Product
-from scrape.scraper import scrape_search
+from listings.models import Listing
+from scrape.scraper import Scraper
+
+stores = [
+    {'name': 'Walmart', 'domain': 'walmart', 'pattern': r'(?<=\/)\d{9}$'},
+    {'name': 'Best Buy', 'domain': 'bestbuy', 'pattern': r'(?<=\/)\d{7}(?=(\.p)$)'},
+    {'name': 'Target', 'domain': 'target', 'pattern': r'(?<=(\/A-))\d{8}$'}
+]
+
+def get_sku(pattern, url_path):
+    try:
+        sku = re.search(pattern, url_path).group(0)
+    except AttributeError:
+        sku = None
+    
+    return sku   
 
 def search(request):
     query = request.GET.get('query', None)
@@ -15,13 +33,34 @@ def search(request):
         
         try:
             validator(query)
-            
-            product = scrape_search(urlparse(query))
 
-            if product:
-                return redirect(product)
-            else:
-                return render(request,'search/search.html',{})
+            url = urlparse(query)
+
+            domain = url.netloc.split('.')[-2]
+
+            store = next(store for store in stores if store.get('domain') == domain)
+
+            vendor_name = store.get('name')
+        
+            sku = get_sku(store.get('pattern'), url.path)
+
+            if vendor_name:
+                one_hour_ago = timezone.now() - timedelta(minutes=1)
+
+                listing = Listing.objects.filter(
+                    updated_time__lte=one_hour_ago,
+                    vendor__name=vendor_name,
+                    sku=sku
+                )
+
+                if listing.exists():
+                    return redirect(listing.first().product)
+
+                scraper = Scraper(vendor_name)
+                scraper.scrape('url', sku)
+                return redirect(scraper.product)
+
+            return render(request,'search/search.html',{})
         
         except ValidationError:
             pass
