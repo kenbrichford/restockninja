2b43240ef9dcfb13f7aa2844979bf94b1774f9bd
Kenning Brichford <kenning@brichford.com>
2020-04-25 18:49:24 -0400

    Add image carousel to product page

---
+++ products/static/products/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.product-images').slick({
+        autoplay: true,
+        dots: true,
+        prevArrow: '.prev',
+        nextArrow: '.next',
+    });
+});
