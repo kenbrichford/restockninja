921c0f6137ca2b1ae160e6972332d598cec0e4ef
Kenning Brichford <kenning@brichford.com>
2020-04-26 08:43:59 -0400

    Change product to products in urls

--- restockninja/urls.py
+++ restockninja/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('product/', include('products.urls')),
+    path('products/', include('products.urls')),
     path('search', include('search.urls')),
 ]
 
