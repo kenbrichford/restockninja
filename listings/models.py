ad5f6d349bc5cac6043e43f49f25ebd2e93e5f6d
Kenning Brichford <kenning@brichford.com>
2020-04-26 20:46:55 -0400

    Increase length of listing url to 500 characters

--- listings/models.py
+++ listings/models.py
@@ -6,7 +6,7 @@ class Listing(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     vendor = models.ForeignKey('Vendor', on_delete=models.CASCADE)
     sku = models.CharField(max_length=20)
-    url = models.URLField(max_length=300, blank=True)
+    url = models.URLField(max_length=500, blank=True)
     updated_time = models.DateTimeField(auto_now=True)
 
     class Meta:
