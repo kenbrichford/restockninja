b8d6643b1cc0ec4d34c88a239da38617e7099a1d
Kenning Brichford <kenning@brichford.com>
2020-05-05 15:22:57 -0400

    Add urls for deleting alerts and emails

--- alerts/urls.py
+++ alerts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
-from .views import create_alert, confirm_alert
+from .views import create_alert, confirm_alert, delete_alert, delete_email
 
 urlpatterns = [
-    path('new/', create_alert, name='create_alert'),
-    re_path(r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', confirm_alert, name='confirm_alert')
+    path('new/', create_alert, name='create-alert'),
+    re_path(r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', confirm_alert, name='confirm-alert'),
+    re_path(r'^delete/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', delete_alert, name='delete-alert'),
+    re_path(r'^delete-all/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', delete_email, name='delete-email'),
 ]
