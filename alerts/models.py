9c09c76508ec3435fb0f10856aa5a50139bc0376
Kenning Brichford <kenning@brichford.com>
2020-05-05 15:18:49 -0400

    Remove verify option from alerts model and add email frequency and time last notified

--- alerts/models.py
+++ alerts/models.py
@@ -8,8 +8,21 @@ class Email(models.Model):
         return self.address
 
 class Alert(models.Model):
+    FREQUENCIES = [
+        (604800, 'Weekly'),
+        (86400, 'Daily'),
+        (43200, 'Two per day'),
+        (28800, 'Three per day'),
+        (3600, 'Hourly'),
+        (0, 'All updates'),
+    ]
+
     email = models.ForeignKey('Email', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    verify = models.BooleanField(default=True)
     is_confirmed = models.BooleanField(default=False)
-    created_time = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    email_frequency = models.PositiveIntegerField(
+        choices=FREQUENCIES,
+        default=86400,
+        )
+    last_notified = models.DateTimeField(null=True)
