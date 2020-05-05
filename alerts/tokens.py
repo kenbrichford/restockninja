568e8870c0e6cadeec09f11bee8df06622d54f52
Kenning Brichford <kenning@brichford.com>
2020-05-05 15:22:06 -0400

    Add token generators for deleting alerts and email addresses

--- alerts/tokens.py
+++ alerts/tokens.py
@@ -8,4 +8,18 @@ class AlertConfirmationTokenGenerator(PasswordResetTokenGenerator):
             six.text_type(alert.is_confirmed)
         )
 
+class AlertDeleteTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, alert, timestamp):
+        return (
+            six.text_type(alert.pk) + six.text_type(timestamp)
+        )
+
+class EmailDeleteTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, email, timestamp):
+        return (
+            six.text_type(email.pk) + six.text_type(timestamp)
+        )
+
 alert_confirmation_token = AlertConfirmationTokenGenerator()
+alert_delete_token = AlertDeleteTokenGenerator()
+email_delete_token = EmailDeleteTokenGenerator()
