9cc230b87a87f51bd6eb4a52ac72fab4d13c650f
Kenning Brichford <kenning@brichford.com>
2020-07-10 13:49:45 -0400

    Increase length of brand name in database

--- products/models.py
+++ products/models.py
@@ -81,7 +81,7 @@ class Category(MPTTModel, Base):
         return super(Category, self).save(*args, **kwargs)
 
 class Brand(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
