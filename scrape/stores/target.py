6dba2bc466519495851ab0c1689af28bd524d4dc
Kenning Brichford <kenning@brichford.com>
2020-07-10 13:50:19 -0400

    Fix failing target api url and data parsing

--- scrape/stores/target.py
+++ scrape/stores/target.py
@@ -12,13 +12,13 @@ class Target:
         keyword = kwargs.get('keyword', None)
 
         params = {
-            'excludes': 'esp',
+            'excludes': 'bulk_ship,rating_and_review_reviews,question_answer_statistics,promotion,rating_and_review_statistics,esp',
             'key': os.getenv('TARGET_API_KEY'),
             'count': 10
         }
 
         if skus:
-            endpoint = 'https://redsky.target.com/v1/plp/collection/%s' % skus
+            endpoint = 'https://redsky.target.com/v2/pdp/tcin/%s' % skus
         elif upc or keyword:
             endpoint = 'https://redsky.target.com/v1/plp/search'
             params['keyword'] = upc if upc else keyword
@@ -26,20 +26,38 @@ class Target:
         return Link(endpoint, params)
 
     def get_items(self, data):
-        response = data.get('search_response', {}).get('items', {}).get('Item')
-        if response and not response[0].get('error_message'):
-            return response
+        if isinstance(data, list):
+            return data
+        elif data.get('product'):
+            if data.get('product').get('item'):
+                return [data]
+        else:
+            response = data.get('search_response', {}).get('items', {}).get('Item')
+            if response and not response[0].get('error_message'):
+                return response
             
         return []
     
     def parse_product_data(self, item):
-        name = html.unescape(item.get('title'))
-        brand = item.get('brand')
-        category = [item.get('merch_class').title()]
-        upc = item.get('upc')
-        if item.get('images'):
-            thumbnail = item.get('images')[0].get('base_url')
-            thumbnail += item.get('images')[0].get('primary')
+        if item.get('product'):
+            item = item.get('product').get('item')
+            name = item.get('product_description', {}).get('title')
+            brand = item.get('product_brand', {}).get('brand', '')
+            category = [item.get('product_classification', {}).get('item_type', {}).get('name').title()]
+            upc = item.get('upc')
+            if item.get('enrichment', {}).get('images'):
+                thumbnailData = item.get('enrichment').get('images')[0]
+                thumbnail = thumbnailData.get('base_url')
+                thumbnail += thumbnailData.get('primary')
+        else:
+            name = html.unescape(item.get('title'))
+            brand = item.get('brand')
+            category = [item.get('merch_class').title()]
+            upc = item.get('upc')
+            if item.get('images'):
+                thumbnail = item.get('images')[0].get('base_url')
+                thumbnail += item.get('images')[0].get('primary')
+        
         variants = []
 
         return Product(name, brand, category, upc, thumbnail, variants)
@@ -47,30 +65,49 @@ class Target:
     def parse_image_data(self, item):
         images = []
 
-        if item.get('images'):
-            base_url = item.get('images')[0].get('base_url')
-            primary = base_url + item.get('images')[0].get('primary')
+        if item.get('product'):
+            item = item.get('product').get('item')
+            if item.get('enrichment', {}).get('images'):
+                imageData = item.get('enrichment').get('images')[0]
+                base_url = imageData.get('base_url')
+                primary = base_url + imageData.get('primary')
 
-            images.append({'url': primary, 'primary': True})
-            
-            if item.get('images')[0].get('alternate_urls'):
-                for image in item.get('images')[0].get('alternate_urls'):
-                    images.append({'url': base_url + image, 'primary': False})
+                images.append({'url': primary, 'primary': True})
+
+                if imageData.get('alternate_urls'):
+                    for image in imageData.get('alternate_urls'):
+                        images.append({'url': base_url + image, 'primary': False})
+        else:
+            if item.get('images'):
+                base_url = item.get('images')[0].get('base_url')
+                primary = base_url + item.get('images')[0].get('primary')
+
+                images.append({'url': primary, 'primary': True})
+                
+                if item.get('images')[0].get('alternate_urls'):
+                    for image in item.get('images')[0].get('alternate_urls'):
+                        images.append({'url': base_url + image, 'primary': False})
 
         return images[:5]
     
     def parse_listing_data(self, item):
-        sku = item.get('tcin')
+        if item.get('product'):
+            sku = item.get('product').get('item').get('tcin')
+        else:
+            sku = item.get('tcin')
         url = 'https://www.target.com%s' % item.get('url')
 
         return Listing(sku, url)
 
     def parse_price_data(self, item):
-        if item.get('availability_status') == 'IN_STOCK':
+        if item.get('product') and item.get('product').get('available_to_promise_network', {}).get('availability_status') == 'IN_STOCK':
+            price = item.get('product').get('price', {}).get('offerPrice', {}).get('price') * 100
+            shipping = 0 if price > self.free_shipping_minimum else None
+            available = True
+        elif item.get('availability_status') and item.get('availability_status') == 'IN_STOCK':
             price = item.get('offer_price').get('price') * 100
             shipping = 0 if price > self.free_shipping_minimum else None
             available = True
-        
         else:
             price = None
             shipping = None
